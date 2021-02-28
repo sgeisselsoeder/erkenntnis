540969abfcc675eddeef3521e86529908f46cfdc
Stefan Geißelsöder <stefangeisselsoeder-github@yahoo.de>
2021-02-28 20:37:19 +0100

    adds test

---
+++ tests/erkenntnis/test_world.py
@@ -0,0 +1,17 @@
+from erkenntnis.world import World
+
+
+def test_world():
+    my_world = World()
+    my_world.add_thing()
+    my_world.add_thing()
+
+    my_world.add_agent()
+    my_world.add_agent()
+
+    my_world.print()
+
+    for i in range(10):
+        my_world.run(time_delta=0.1)
+
+    my_world.print()
