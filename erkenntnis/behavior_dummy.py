978f42dfbe01cb3d89d8823f061d27d90ffb66fb
Stefan Geißelsöder <stefan@geisselsoeder.com>
2021-03-01 17:19:52 +0100

    fixes dummy memory, adds forgetting of old memories

--- erkenntnis/behavior_dummy.py
+++ erkenntnis/behavior_dummy.py
@@ -7,12 +7,21 @@ class Behavior_dummy(Behavior):
         self.last_perceptions = list()
         self.last_actions = list()
 
-    def think(self, perception):
+    def _remember(self, perception, action):
         self.last_perceptions.append(perception)
+        self.last_actions.append(action)
+        self._forget_old()
 
+
+    def _forget_old(self, memory_length: int = 10):
+        self.last_perceptions = self.last_perceptions[-memory_length:]
+        self.last_actions = self.last_actions[-memory_length:]
+
+    def think(self, perception):
         action = get_action_accelerate(direction=np.array([2.0 * np.random.random() - 1.0,
                                                            2.0 * np.random.random() - 1.0,
                                                            0.0]))
-        self.last_actions.append(action)
+        
+        self._remember(perception=perception, action=action)
 
         return action
