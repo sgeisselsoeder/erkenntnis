cd56f298a5b11cc62389d3b88effba82649bd2bf
Stefan Geißelsöder <stefangeisselsoeder-github@yahoo.de>
2021-03-03 10:03:21 +0100

    refactors thing creation

--- erkenntnis/agents_available.py
+++ erkenntnis/agents_available.py
@@ -1,32 +1,42 @@
+import numpy as np
 from .agent import Agent
 from .behavior_dummy import Behavior_dummy
 from .behavior_sheep_hardcoded import Behavior_sheep_hardcode
 from .behavior_wolf_hardcoded import Behavior_wolf_hardcode
 from .behavior_ape_ml import Behavior_ape_ml
+from .utils import random_position
 
 
-def get_dummy_agent(position, velocity=None, radius=None, health: int = None, perception_radius=None, max_speed=3.0):
+def new_dummy_agent(position: np.ndarray = None, velocity=None, radius=None, health: int = None, perception_radius=None, max_speed=3.0):
+    if position is None:
+        position = random_position()
     agent = Agent(behavior=Behavior_dummy(), position=position, velocity=velocity,
                   radius=radius, health=health, perception_radius=perception_radius)
     agent.type_properties = "dummy"
     return agent
 
 
-def get_sheep_agent(position, velocity=None, radius=None, health: int = None, perception_radius=None, max_speed=1.5):
+def new_sheep(position: np.ndarray = None, velocity=None, radius=None, health: int = None, perception_radius=None, max_speed=1.5):
+    if position is None:
+        position = random_position()
     agent = Agent(behavior=Behavior_sheep_hardcode(action_distance=max_speed), position=position, velocity=velocity,
                   radius=radius, health=health, perception_radius=perception_radius, max_speed=1.5)
     agent.type_properties = "sheep"
     return agent
 
 
-def get_wolf_agent(position, velocity=None, radius=None, health: int = None, perception_radius=None, max_speed=2.0):
+def new_wolf(position: np.ndarray = None, velocity=None, radius=None, health: int = None, perception_radius=None, max_speed=2.0):
+    if position is None:
+        position = random_position()
     agent = Agent(behavior=Behavior_wolf_hardcode(action_distance=max_speed), position=position, velocity=velocity,
                   radius=radius, health=health, perception_radius=perception_radius, max_speed=max_speed)
     agent.type_properties = "wolf"
     return agent
 
 
-def get_ape_agent(position, velocity=None, radius=None, health: int = None, perception_radius=None, max_speed=2.5):
+def new_ape(position: np.ndarray = None, velocity=None, radius=None, health: int = None, perception_radius=None, max_speed=2.5):
+    if position is None:
+        position = random_position()
     agent = Agent(behavior=Behavior_ape_ml(action_distance=max_speed), position=position, velocity=velocity,
                   radius=radius, health=health, perception_radius=perception_radius, max_speed=max_speed)
     agent.type_properties = "ape"
