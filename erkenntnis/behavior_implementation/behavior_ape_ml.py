ac7b3f7605516c5303082e0cd8b9d182712e51a1
Stefan Geißelsöder <stefangeisselsoeder-github@yahoo.de>
2021-03-03 11:41:55 +0100

    updates the behavior of apes (to do nothing)

--- erkenntnis/behavior_implementation/behavior_ape_ml.py
+++ erkenntnis/behavior_implementation/behavior_ape_ml.py
@@ -1,24 +1,58 @@
-from ..agent import Behavior
+from ..behavior_simple_memory import Behavior_simple_memory
 from ..world_actions import *
+from ..analyze_perception import split_perception_by_type
 from ..utils import random_position
 
 
-class Behavior_ape_ml(Behavior):
-    def __init__(self):
-        self.last_perceptions = list()
-        self.last_actions = list()
-        self.memory_length = 100
+class Behavior_ape_ml(Behavior_simple_memory):
+    def __init__(self, action_distance: float):
+        super().__init__(memory_length=100)
+        self.current_target_direction = None
+        self.action_distance = action_distance
 
-    def _remember(self, perception, action):
-        self.last_perceptions.append(perception)
-        self.last_actions.append(action)
-        self._forget_old()
+    def think(self, perception, messages):
+        return None
 
-    def _forget_old(self):
-        self.last_perceptions = self.last_perceptions[-self.memory_length:]
-        self.last_actions = self.last_actions[-self.memory_length:]
+        action = None
+        action_cause = None
 
-    def think(self, perception):
-        action = action_accelerate(direction=random_position())
-        self._remember(perception=perception, action=action)
+        for message in messages:
+            if message["message"] == "malus":
+                action = action_remove_malus()
+                action_cause = message["from"]
+
+        # TODO: react to other messages as well
+
+        # Without messages, we analze what we see
+        split_perception = split_perception_by_type(perception=perception)
+
+        if "sheep" in split_perception:
+            nearest_sheep = split_perception["sheep"][0]
+            direction_to_nearest = nearest_sheep.position
+
+            eating_distance = self.action_distance
+            current_distance = vector_length(direction_to_nearest)
+            if current_distance < eating_distance:
+                action = action_eat(direction=direction_to_nearest)
+            else:
+                action = action_accelerate(direction=direction_to_nearest)
+            action_cause = nearest_sheep.unique_properties
+
+        elif "ape" in split_perception:
+            for wolf in split_perception["ape"]:
+                if wolf.malus:
+                    action = action_inform_malus(direction=wolf.position)
+                    action_cause = wolf.unique_properties
+                    break
+            if action is None:
+                nearest_wolf = split_perception["wolf"][0]
+                action = action_accelerate(direction=nearest_wolf.velocity)
+                action_cause = nearest_wolf.unique_properties
+
+        else:
+            # we might want to keep a direction, but random for now
+            self.current_target_direction = random_position()
+            action = action_accelerate(direction=self.current_target_direction)
+
+        self._remember(perception=perception, messages=messages, action=action, action_cause=action_cause)
         return action
