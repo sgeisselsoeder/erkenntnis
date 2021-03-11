2e42f8a65c32ef95df74f140989af8de8723f471
Stefan Geißelsöder <stefangeisselsoeder-github@yahoo.de>
2021-03-11 15:48:02 +0100

    adds dummies and regrows grass in main.py

--- main.py
+++ main.py
@@ -15,23 +15,29 @@ my_world.add(new_stone(position=None))
 for i in range(300):
     my_world.add(new_grass(), position=random_position(scale=initial_world_scale))
 
-for i in range(20):
+for i in range(40):
     my_world.add(new_sheep(), position=random_position(scale=0.8*initial_world_scale))
 
 for i in range(3):
     my_world.add(new_wolf(), position=random_position(scale=0.6*initial_world_scale))
 
 for i in range(5):
-    my_world.add(new_monkey(), position=random_position(scale=0.4*initial_world_scale))
+    my_world.add(new_monkey(log=True), position=random_position(scale=0.4*initial_world_scale))
+
+for i in range(10):
+    my_world.add(new_dummy_agent(), position=random_position(scale=initial_world_scale))
 
 my_world.print()
 my_world.map(resolution=map_resultion, fixed_boundary=map_boundary, plotstyle="dense")
 
 time.sleep(2)
 
-for i in range(1000):
+for i in range(3000):
     my_world.run(time_delta=0.3)
-    my_world.map(resolution=map_resultion, fixed_boundary=map_boundary, plotstyle="dense")
+    # my_world.map(resolution=map_resultion, fixed_boundary=map_boundary, plotstyle="dense")
+    if np.random.random() < 0.5:
+        my_world.add(new_grass(), position=random_position(scale=initial_world_scale*1.8))
+    my_world.map(resolution=map_resultion, fixed_boundary=map_boundary)
 
 my_world.print()
-my_world.map(resolution=map_resultion, fixed_boundary=map_boundary, plotstyle="dense")
+my_world.map(resolution=map_resultion, fixed_boundary=map_boundary)
