a692c8b122a8ce5b77b867b8d35baade6a41e931
Stefan Geißelsöder <stefan@geisselsoeder.com>
2021-04-05 15:18:41 +0200

    logging tests adapted to reduced float accuracy

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -19,8 +19,8 @@ def test_logging_loadable():
 def test_logging_correct_values():
     my_world = World(agent_health_decline=0.0, malus_propability=0.0)
 
-    position_1 = 0.456123
-    position_2 = 0.789321
+    position_1 = 0.4561
+    position_2 = 0.7894
     my_world.add(new_stone(), position=np.array([position_1, position_2, 0.0]))
     # my_world.add(new_dummy_agent(), position=np.array([0.0, 0.0, 0.0]))
     my_world.add(new_monkey(log=True), position=np.array([0.0, 0.0, 0.0]))
@@ -38,5 +38,19 @@ def test_logging_correct_values():
     # therefore the shape of the logged data can be smaller ...
     # TODO: fix, also logging without thinking.
     assert(logdata.shape[0] == number_time_steps)
-    positions = [position_1, position_2]
-    assert(np.sum(np.isin(element=logdata[0], test_elements=positions)) == len(positions))
+
+    entry = np.around(position_1, decimals=3)
+    found = False
+    for number in logdata[0]:
+        if np.around(number, decimals=3) == entry:
+            found = True
+            break
+    assert(found)
+
+    entry = np.around(position_2, decimals=3)
+    found = False
+    for number in logdata[0]:
+        if np.around(number, decimals=3) == entry:
+            found = True
+            break
+    assert(found)
