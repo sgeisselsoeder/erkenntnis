a978acb886ddc156538488ce1b3a2a58d8639cba
Stefan Geißelsöder <stefan@geisselsoeder.com>
2021-04-05 15:16:28 +0200

    adds assert:approx_equal function (reduced float accuracy means == fails often)

--- erkenntnis/utils.py
+++ erkenntnis/utils.py
@@ -19,3 +19,15 @@ def vector_length(v: np.array):
     length = np.sqrt(v[0] * v[0] + v[1] * v[1])
     return length
     # return np.linalg.norm(v)
+
+
+def assert_approx_equal(a, b, rtol=0.001):
+    if isinstance(a, np.ndarray):
+        assert(np.allclose(a, b))
+    elif isinstance(a, float) or isinstance(a, np.float) or isinstance(a, np.float32) or isinstance(a, np.float64):
+        if a == 0.0 and b == 0.0:
+            return
+        assert(abs(a - b) / max(abs(a), abs(b)) < rtol)
+    else:
+        assert(np.all(a == b))
+    
